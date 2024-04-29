75d4fe58ce758da43b91815d4bdd00f3d499b29c
axmed97 <axmed.dev.13@gmail.com>
2024-04-29 21:52:24 +0400

    Add project files.

---
+++ Core/DataAccess/IRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Core.DataAccess
+{
+    public interface IRepositoryBase<TEntity>
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Entities/Concrete/ProductPhoto.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Common;
+
+namespace Entities.Concrete
+{
+    public class ProductPhoto : BaseEntity
+    {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
